d39b8c489f1b1cd7ea58f0f45d1ce5fb55d36d55
PhilippeOberti <philippe.oberti@elastic.co>
2025-06-03 11:36:14 -0500

    [Security Solution] update severity colors

--- x-pack/solutions/security/plugins/security_solution/public/common/utils/risk_color_palette.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/common/utils/risk_color_palette.tsx
@@ -9,36 +9,13 @@ import type { EuiThemeComputed } from '@elastic/eui';
 import { useEuiTheme } from '@elastic/eui';
 import type { Severity } from '@kbn/securitysolution-io-ts-alerting-types';
 import { useMemo } from 'react';
-import {
-  RISK_COLOR_CRITICAL,
-  RISK_COLOR_HIGH,
-  RISK_COLOR_LOW,
-  RISK_COLOR_MEDIUM,
-} from '../constants';
 
-// Temporary solution until we have a decision for color palette https://github.com/elastic/kibana/issues/203387
-export const SEVERITY_COLOR = {
-  low: '#54B399',
-  medium: '#D6BF57',
-  high: '#DA8B45',
-  critical: '#E7664C',
-} as const;
-
-const isAmsterdam = (euiThemeName: string) => {
-  return euiThemeName?.toLowerCase().includes('amsterdam');
-};
-
-export const getRiskSeverityColors = (euiTheme: EuiThemeComputed) => {
-  if (euiTheme && isAmsterdam(euiTheme.themeName)) {
-    return {
-      low: RISK_COLOR_LOW,
-      medium: RISK_COLOR_MEDIUM,
-      high: RISK_COLOR_HIGH,
-      critical: RISK_COLOR_CRITICAL,
-    };
-  }
-  return SEVERITY_COLOR;
-};
+export const getRiskSeverityColors = (euiTheme: EuiThemeComputed) => ({
+  low: euiTheme.colors.severity.neutral,
+  medium: euiTheme.colors.severity.warning,
+  high: euiTheme.colors.severity.risk,
+  critical: euiTheme.colors.severity.danger,
+});
 
 export const useRiskSeverityColors = (): Record<Severity, string> => {
   const { euiTheme } = useEuiTheme();
