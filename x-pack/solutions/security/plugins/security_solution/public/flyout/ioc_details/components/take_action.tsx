aa0fa856d311354300c2c3f6bbbf6c7a9bd62bdd
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-10-03 23:23:17 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- x-pack/solutions/security/plugins/security_solution/public/flyout/ioc_details/components/take_action.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/flyout/ioc_details/components/take_action.tsx
@@ -10,14 +10,10 @@ import { EuiButton, EuiContextMenuPanel, EuiPopover, useGeneratedHtmlId } from '
 import { FormattedMessage } from '@kbn/i18n-react';
 import { useIOCDetailsContext } from '../context';
 import { canAddToBlockList } from '../../../threat_intelligence/modules/block_list/utils/can_add_to_block_list';
-import {
-  AddToBlockListContextMenu
-} from '../../../threat_intelligence/modules/block_list/components/add_to_block_list';
+import { AddToBlockListContextMenu } from '../../../threat_intelligence/modules/block_list/components/add_to_block_list';
 import { AddToNewCase } from '../../../threat_intelligence/modules/cases/components/add_to_new_case';
 import { AddToExistingCase } from '../../../threat_intelligence/modules/cases/components/add_to_existing_case';
-import {
-  InvestigateInTimelineContextMenu
-} from '../../../threat_intelligence/modules/timeline/components/investigate_in_timeline';
+import { InvestigateInTimelineContextMenu } from '../../../threat_intelligence/modules/timeline/components/investigate_in_timeline';
 
 export const TAKE_ACTION_BUTTON_TEST_ID = 'tiIndicatorFlyoutTakeActionButton';
 export const INVESTIGATE_IN_TIMELINE_TEST_ID = 'tiIndicatorFlyoutInvestigateInTimelineContextMenu';
