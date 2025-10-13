9af824cb2ee019c9b57cfe11a499c49c78aacaa0
PhilippeOberti <philippe.oberti@elastic.co>
2025-10-13 14:49:05 -0500

    [Security Solution] remove unnecessary redux slice for experimental feature flags

--- x-pack/solutions/security/plugins/security_solution/common/experimental_features.ts
+++ x-pack/solutions/security/plugins/security_solution/common/experimental_features.ts
@@ -226,4 +226,4 @@ export const parseExperimentalConfigValue = (
   };
 };
 
-export const getExperimentalAllowedValues = (): string[] => [...allowedKeys];
+export const getExperimentalAllowedValuesKeys = (): string[] => [...allowedKeys];
